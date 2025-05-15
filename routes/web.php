1e9b68b7d467931ed891e9de7236b6c5c96c6907
Fatih Ates <fatihmac@Fatih-MacBook-Pro.local>
2025-05-15 19:59:41 +0300

    cafe uuid eklendi

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,9 @@ Route::get('/spotify/login', [SpotifyController::class, 'login']);
 Route::get('/spotify/logout', [SpotifyController::class, 'logout'])->name('spotify.logout');
 Route::get('/spotify/callback', [SpotifyController::class, 'callback']);
 Route::get('/spotify/playing', [SpotifyController::class, 'currentlyPlaying']);
-Route::get('/spotify/show', function () {
+Route::get('/spotify/show/{uuid?}', function ($uuid = null) {
+    if (!$uuid){
+        echo "Cafe bulunamadı!";
+    }
     return view('spotify.show');
 });
